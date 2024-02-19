7f9dcf6e0270b1fa57bddb95f88b44db6a40d9c7
Your NameE <you@example.com>
2024-02-19 10:42:43 -0600

    -

--- pme/pme.c
+++ pme/pme.c
@@ -81,6 +81,7 @@ static void pme_sweep(struct problem_spec *spec, double T, int n, int s, int m, 
     //Time step and space step
     double tstep = T/2*s;
     double xstep = 2/n;
+    printf("lf", xstep);
     double r = (tstep*2)/(2*(xstep)*(xstep));
     //geomview stuff
     if ((fp = fopen(gv_filename, "w"))==NULL)
