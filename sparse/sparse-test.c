1330f4f3fa57af4bba928f62319d7afd11d12ac9
Elliott Love <eelove@uark.edu>
2024-04-04 08:58:22 -0500

    sparse

--- sparse/sparse-test.c
+++ sparse/sparse-test.c
@@ -1,33 +1,39 @@
 #include "sparse.h"
 #include "array.h"
 #include <stdio.h>
+#include "xmalloc.h"
 
 int main()
 {
     //creating a
-    double **a;
-    make_matrix(a, 4, 5);
-    a[0][0] = 0; a[0][1] = 7; a[0][2] = 0; a[0][3] = 0; a[0][4] = 1;
-    a[1][0] = 0; a[1][1] = 4; a[1][2] = 0; a[1][3] = 3; a[1][4] = 0;
-    a[2][0] = 6; a[2][1] = 6; a[2][2] = 5; a[2][3] = 1; a[2][4] = 4;
-    a[3][0] = 5; a[3][1] = 5; a[3][2] = 0; a[3][3] = 0; a[3][4] = 0;
+    double **a = malloc(4 * sizeof(double *));
+    for (int i = 0; i < 4; i++) 
+    {
+        a[i] = malloc(5 * sizeof(double));
+    }   
+    
+    a[0][0] = 0.0; a[0][1] = 7.0; a[0][2] = 0.0; a[0][3] = 0.0; a[0][4] = 1.0;
+    a[1][0] = 0.0; a[1][1] = 4.0; a[1][2] = 0.0; a[1][3] = 3.0; a[1][4] = 0.0;
+    a[2][0] = 6.0; a[2][1] = 6.0; a[2][2] = 5.0; a[2][3] = 1.0; a[2][4] = 4.0;
+    a[3][0] = 5.0; a[3][1] = 5.0; a[3][2] = 0.0; a[3][3] = 0.0; a[3][4] = 0.0;
 
     //counting the number of non-zero entries 
     int nnz = 0;
+    
     for (int i=0; i<4; i++)
     {
         for (int j=0; j<5; j++)
         {
             printf("%g", a[i][j]);
-            if (abs(a[i][j]<.0001))
+            if (abs(a[i][j]>.0001))
             {
                 nnz = nnz + 1;
             }
         }
         printf("\n");
+        printf("%d", nnz);
     }
     printf("%d", nnz);
-    printf("The number of non-zero entries in a is %d", nnz);
     printf("%d", nnz);
 
     //Creating Ai, Ap, Ax
@@ -43,7 +49,12 @@ int main()
     print_vector("%d", Ai, nnz);
     print_vector("%d", Ap, 6);
 
-    
+    for (int i = 0; i < 4; i++)
+    {
+        free(a[i]);
+    }
+    free(a);
+
 
     return 0;
 
